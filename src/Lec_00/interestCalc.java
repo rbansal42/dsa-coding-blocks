9d9e6b4ffefeeca84d0737d7fe584df46269dceb
Rahul Bansal <42.rahulbansal@gmail.com>
2023-09-12 17:35:49 +0530

    Program to find the interest amount on principle

---
+++ src/Lec_00/interestCalc.java
@@ -0,0 +1,16 @@
+package Lec_00;
+
+public class interestCalc {
+    public static void main(String[] args){
+        int amount = 1000;
+        int roi = 10;
+        int time = 1;
+
+        double interest = (amount * roi * time)/100;
+
+        System.out.println(interest);
+    }
+}
+
+
+
