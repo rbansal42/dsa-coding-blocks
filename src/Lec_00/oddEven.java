9fa0635878e4636194f8beeb5aa09b76d1363777
Rahul Bansal <42.rahulbansal@gmail.com>
2023-09-12 17:44:02 +0530

    Program to find whether a number is Odd or Even

---
+++ src/Lec_00/oddEven.java
@@ -0,0 +1,16 @@
+package Lec_00;
+
+public class oddEven {
+    public static void main(String[] args) {
+
+        int num = 16;
+
+        if (num%2 == 0){
+            System.out.println("Even");
+        }
+        else {
+            System.out.println("Odd");
+        }
+
+    }
+}
