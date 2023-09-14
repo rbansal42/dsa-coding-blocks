0dec10ec285c7f7ec05db18bec739af23c245431
Rahul Bansal <42.rahulbansal@gmail.com>
2023-09-14 16:53:09 +0530

    Program to count and print

---
+++ src/Lec_01/counting.java
@@ -0,0 +1,11 @@
+package Lec_01;
+
+public class counting {
+    public static void main(String[] args) {
+        int n = 5, count = 1;
+        while (count <= n) {
+            System.out.print(count + " ");
+            count++;
+        }
+    }
+}
