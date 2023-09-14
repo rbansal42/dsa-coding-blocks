13c36df851913af873af9f2b5d18407b0b631c67
Rahul Bansal <42.rahulbansal@gmail.com>
2023-09-14 17:04:47 +0530

    Program to find sum of N Natural Numbers

---
+++ src/Lec_01/sumOfN_NaturalNo.java
@@ -0,0 +1,12 @@
+package Lec_01;
+
+public class sumOfN_NaturalNo {
+    public static void main(String[] args) {
+        int n = 5, i = 0, sum = 0;
+        while (i<5){
+            sum += i;
+            i++;
+        }
+        System.out.println(sum);
+    }
+}
