763944cf5177c47758dbb128aaa310f2613ee280
Rahul Bansal <42.rahulbansal@gmail.com>
2023-09-14 17:29:12 +0530

    Fixed start for iterator. Natural no start from 1, not 0.

--- src/Lec_01/sumOfN_NaturalNo.java
+++ src/Lec_01/sumOfN_NaturalNo.java
@@ -2,8 +2,8 @@ package Lec_01;
 
 public class sumOfN_NaturalNo {
     public static void main(String[] args) {
-        int n = 5, i = 0, sum = 0;
-        while (i<5){
+        int n = 5, i = 1, sum = 0;
+        while (i<=5){
             sum += i;
             i++;
         }
