bf73ddf05d575e278247dad844025d9542625886
Rahul Bansal <42.rahulbansal@gmail.com>
2023-09-14 16:52:47 +0530

    Program to understand loops

---
+++ src/Lec_01/loops.java
@@ -0,0 +1,11 @@
+package Lec_01;
+
+public class loops {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i!= 100) {
+            System.out.println(i + " aag");
+            i++;
+        }
+    }
+}
