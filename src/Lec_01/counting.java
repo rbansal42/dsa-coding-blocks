725b7e3b8156a7236759dfef68997de60070f09a
Rahul Bansal <42.rahulbansal@gmail.com>
2023-09-14 17:04:33 +0530

    Added a different approach to solve this question

--- src/Lec_01/counting.java
+++ src/Lec_01/counting.java
@@ -1,11 +1,24 @@
 package Lec_01;
 
+//public class counting {
+//    public static void main(String[] args) {
+//        int n = 5, count = 1;
+//        while (count <= n) {
+//            System.out.print(count + " ");
+//            count++;
+//        }
+//    }
+//}
+
+
+// Different approach
 public class counting {
     public static void main(String[] args) {
-        int n = 5, count = 1;
-        while (count <= n) {
-            System.out.print(count + " ");
+        int n = 5, count = 0;
+        while (count < n) {
             count++;
+            System.out.print(count + " ");
+
         }
     }
 }
