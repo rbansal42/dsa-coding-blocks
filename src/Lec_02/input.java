aebe85514e33391f2f3465cd3f6fab46a7ebff55
Rahul Bansal <42.rahulbansal@gmail.com>
2023-09-15 16:39:07 +0530

    Introduction on how to accept inputs in JAVA

---
+++ src/Lec_02/input.java
@@ -0,0 +1,12 @@
+package Lec_02;
+
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        System.out.println("Input received is " + i);
+    }
+
+}
