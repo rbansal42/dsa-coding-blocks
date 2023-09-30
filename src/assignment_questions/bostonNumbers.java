5a8d62d261090afed7d650c7d2dcfb8036393cac
Rahul Bansal <42.rahulbansal@gmail.com>
2023-09-30 12:57:29 +0530

    Solved but failes a few test cases

--- src/assignment_questions/bostonNumbers.java
+++ src/assignment_questions/bostonNumbers.java
@@ -1,27 +1,42 @@
 package assignment_questions;
 
+import java.util.Scanner;
+
 public class bostonNumbers {
     public static void main(String[] args) {
-        int num = 1532;
+        Scanner scan = new Scanner(System.in);
+        long num = scan.nextLong();
 
-        int sum = 0;
-        while (num > 0){
-            int digit = num%10;
+        int sumOfPrimeFactors = 0;
+        int sumOfDigits = 0;
 
-            num /= 10;
-            sum += digit;
+        // finding sum of digits
+        // extracting each digit of integer and adding it to sumOfDigits
+        long copy = num;
+        while (copy != 0) {
+            sumOfDigits += copy % 10;
+            copy /= 10;
         }
 
-        int div = 2;
+        // finding sum of prime factors
+        copy = num;     // Resetting copy of num as it will reach 0 after previous loop
+        long i = 2;
+        while (copy != 1) {
 
-        while (num > 1){
-            if (num % div == 0){
-                System.out.println(num);
-                num /= div;
+            if (copy % i == 0) {
+                sumOfPrimeFactors += i;
+                copy = copy / i;
+            } else {
+                i++;
             }
-            else{
-                div++;
-            }
+        }
+
+        // compare sumOfDigits and sumOfPrimeFactors to see if they are equal
+
+        if (sumOfDigits == sumOfPrimeFactors) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
         }
     }
 }
