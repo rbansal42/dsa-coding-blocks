16d8df78e6093ac4c3d529eab92d00248d063c32
Rahul Bansal <42.rahulbansal@gmail.com>
2023-09-30 15:45:23 +0530

    peasy

---
+++ src/assignment_questions/printTable2.java
@@ -0,0 +1,17 @@
+package assignment_questions;
+
+import java.util.Scanner;
+
+public class printTable2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int rangeLimit = scan.nextInt();
+
+        int i = 1;
+        while (i <= rangeLimit){
+            System.out.println(n*i);
+            i++;
+        }
+    }
+}
