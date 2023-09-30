ef52c204bbce87b2eb593ccbeccacc2ca6d38471
Rahul Bansal <42.rahulbansal@gmail.com>
2023-09-30 15:45:47 +0530

    lemony squeezy

---
+++ src/assignment_questions/binaryToDecimal.java
@@ -0,0 +1,22 @@
+package assignment_questions;
+
+import java.util.Scanner;
+
+public class binaryToDecimal {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int binaryNumber = scan.nextInt();
+        int copy = binaryNumber;
+
+        int decimalNumber = 0;
+
+        int order = 0;
+        while (copy != 0) {
+            decimalNumber += Math.pow(2, order) * (copy % 10);
+
+            copy /= 10;
+            order++;
+        }
+        System.out.println(decimalNumber);
+    }
+}
---
+++ src/assignment_questions/lakshayBhaiyaCantCount.java
@@ -0,0 +1,16 @@
+package assignment_questions;
+
+import java.util.Scanner;
+
+public class lakshayBhaiyaCantCount {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        int i = 1;
+        while (i <= n){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
