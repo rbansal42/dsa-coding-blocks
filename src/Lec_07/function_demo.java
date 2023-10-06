893d0d2f25eaec48682d3ae8f69dc0ef144ce923
Rahul Bansal <42.rahulbansal@gmail.com>
2023-10-06 11:09:49 +0530

    Done

---
+++ src/Lec_07/function_demo.java
@@ -0,0 +1,22 @@
+package Lec_07;
+
+import java.util.Scanner;
+
+public class function_demo {
+    public static void main(String[] args) {
+        System.out.println(compare(10, 10));
+
+    }
+
+    public static int addFunction(int a, int b) {
+        return a + b;
+    }
+
+    public static boolean compare(int a, int b) {
+
+        if (a == b){
+            return true;
+        }
+        return a > b;
+    }
+}
